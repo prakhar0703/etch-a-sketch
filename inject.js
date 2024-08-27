d3f104cefe3e2491e47a4062238af9f9c174bfa4
Prakhar <dixit.madhur.07@gmail.com>
2024-08-27 11:47:45 +0530

    Add dimensions button to page

--- inject.js
+++ inject.js
@@ -1,20 +1,48 @@
-let dimensions = prompt("What size grid do you want?");
-const container = document.querySelector("#container");
-for(let i = 0; i < dimensions; i++){
+
+const container = document.querySelector("#container")
+for(let i = 0; i < 16; i++){
     const outerdiv = document.createElement("div");
     outerdiv.classList.add("outerdiv");
     container.appendChild(outerdiv);
-    for(let j = 0; j < dimensions; j++){
+    for(let j = 0; j < 16; j++){
         const innerdiv = document.createElement("div");
         innerdiv.classList.add("innerdiv");
         outerdiv.appendChild(innerdiv);
     }
 }
-const outerdiv = document.querySelector(".outerdiv");
-const innerdiv = document.querySelectorAll(".innerdiv");
-innerdiv.forEach(div => {
-    div.addEventListener("mouseover", () => {div.style.backgroundColor = "black";})
-});
+const innerdiv = document.querySelectorAll(".innerdiv"); //returns nodelist of all divs with class innerdiv
+    innerdiv.forEach(div => {   //iterate through each element in nodellist
+        div.addEventListener("mouseover", () => {div.style.backgroundColor = "black";})
+    });
+
+const dimension_button = document.createElement("button");
+dimension_button.textContent = "Add dimensions";
+dimension_button.addEventListener("click", createGrid);
+document.body.appendChild(dimension_button);
+
+
+
+function createGrid(){
+    
+    let dimensions = prompt("What size grid do you want?");
+    container.innerHTML = "";
+    for(let i = 0; i < dimensions; i++){
+        const outerdiv = document.createElement("div");
+        outerdiv.classList.add("outerdiv");
+        container.appendChild(outerdiv);
+        for(let j = 0; j < dimensions; j++){
+            const innerdiv = document.createElement("div");
+            innerdiv.classList.add("innerdiv");
+            outerdiv.appendChild(innerdiv);
+        }
+    }
+    
+    const innerdiv = document.querySelectorAll(".innerdiv"); //returns nodelist of all divs with class innerdiv
+    innerdiv.forEach(div => {   //iterate through each element in nodellist
+        div.addEventListener("mouseover", () => {div.style.backgroundColor = "black";})
+    });
+}
+
 
    
 
